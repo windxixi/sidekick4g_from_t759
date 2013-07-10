da36471ca8a88659c657f916527d925277b615ee
windxixi <windxixi@gmail.com>
2013-07-10 03:38:34 -0700

    project init from t759

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 64 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#64 PREEMPT Mon Jul 1 07:36:46 PDT 2013"
+#define LINUX_COMPILE_TIME "07:36:46"
+#define LINUX_COMPILE_BY "windxixi"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
